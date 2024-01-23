6d9b9e58490c6db9593bd30ab9f6a98d0a925822
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2024-01-22 18:20:40 -0800

    CRUD operations

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template
-from config import Config
+from config import Myconfig
 from .api.routes import api
 from .site.routes import site
 from .authentication.routes import auth
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db as root_db, login_manager, ma
+from app.models import db as root_db, login_manager, ma
 from flask_cors import CORS
-from helpers import JSONEncoder
+from app.helpers import JSONEncoder
 
 app = Flask(__name__)
 CORS(app)
-
+print("Hello World")
 app.register_blueprint(site)
 app.register_blueprint(auth)
 app.register_blueprint(api)
 
+print(Myconfig.__dict__)
 app.json_encoder = JSONEncoder
-app.config.from_object(Config)
+app.config.from_object(Myconfig)
 root_db.init_app(app)
 login_manager.init_app(app)
 ma.init_app(app)
